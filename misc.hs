0cfaf258cecd03486b4612ca8d365f2075829422
Tyler Weir <tyler.weir@gmail.com>
2017-10-09 06:51:13 -0400

    Create misc.hs

---
+++ misc.hs
@@ -0,0 +1,23 @@
+{- 
+What: 
+Looks up an environment var and sets a default if not defined.
+Usage: 
+env  <- lookupSetting "ENV" Development
+-}
+
+lookupSetting :: Read a => String -> a -> IO a
+lookupSetting env def = do
+    maybeValue <- lookupEnv env
+    case maybeValue of
+        Nothing ->
+            return def
+        Just str ->
+            maybe (handleFailedRead str) return (readMay str)
+  where
+    handleFailedRead str =
+        error $ mconcat
+            [ "Failed to read [["
+            , str
+            , "]] for environment variable "
+            , env
+            ]
